c79a39259ef6c54077d7fc925fe9465169be70ef
bsse23066 <bsse23066@itu.edu.pk>
2026-05-24 05:37:46 +0500

    fixed login lowercase

--- server/controllers/authController.js
+++ server/controllers/authController.js
@@ -4,28 +4,43 @@ const loginUser = async (req, res) => {
 
   try {
 
-    const { username, password } = req.body
+    let { username, password } = req.body
 
-    const user = await User.findOne({ username })
+    // REMOVE SPACES + MAKE LOWERCASE
+    username = username.trim().toLowerCase()
 
+    // FIND USER
+    const user = await User.findOne({
+      username: username
+    })
+
+    // USER NOT FOUND
     if (!user) {
+
       return res.status(400).json({
         message: "User not found"
       })
+
     }
 
+    // PASSWORD CHECK
     if (user.password !== password) {
+
       return res.status(400).json({
         message: "Invalid password"
       })
+
     }
 
+    // SUCCESS
     res.status(200).json({
       message: "Login successful",
       user
     })
 
   } catch (error) {
+
+    console.log(error)
 
     res.status(500).json({
       message: "Server Error"
